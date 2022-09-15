f825ed7e8eb2f262a4c2b72e926f88f5e1defa4d
Ariane França <ariane.franca@uberall.com>
2022-09-15 00:10:14 -0300

    adicionando chamadas e modulos

--- scripts/login.js
+++ scripts/login.js
@@ -3,32 +3,88 @@
 // Nenhum dos campos pode ser vazio/nulo;
 // O email deve ser de um tipo válido (ex: aplicar expressões regulares);
 // O botão de acesso deve ser habilitado apenas quando todos os campos do formulário estiverem validados corretamente.
-window.addEventListener("load", function () {
-    let inputEmail = document.getElementById("inputEmail");
-    let inputPassword = document.getElementById("inputPassword");
-    let buttonSubmit = document.getElementById("submit");
 
-    function normalizarCampos() {
-        inputEmail = inputEmail.value.trim();
-        inputPassword = inputPassword.value.trim();
-        console.log("Email: " + inputEmail)
+//Nenhum dos campos pode ser vazio/nulo;
+
+//O botão de acesso deve ser habilitado
+//apenas quando todos os campos do formulário
+//estiverem validados corretamente.
+//Envio POST Login
+
+const baseUrl = "https://ctd-todo-api.herokuapp.com";
+
+function login({emailEntry, passwordEntry}){
+    let postBodyLogin = {
+        email: emailEntry,
+        password: passwordEntry
+      };
+      
+      fetch(`${baseUrl}/v1/users/login`, {
+        method: "POST",
+        body: JSON.stringify(postBodyLogin),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      })
+        .then((response) => {
+          if (response.status === 201) {
+            console.log("Successfull login");
+            return response.json()
+          }
+        })
+        .then((data) => {
+            console.log(`
+            jwt: ${data.jwt}
+            `);
+           let response = data.jwt
+           return response
+        })
+        .catch((err) => console.log(err));
+}
+
+// email: "asasasas@mail.com",
+// password: "asaasd",
+
+
+
+window.addEventListener("load", function (event) {
+  let inputEmail = document.getElementById("inputEmail");
+  let inputPassword = document.getElementById("inputPassword");
+  let buttonSubmit = document.getElementById("submit");
+  let inputs = document.querySelectorAll("input");
+
+  buttonSubmit.disabled = true;
+
+  function normalizarCampos() {
+    inputEmail = inputEmail.value.trim();
+    inputPassword = inputPassword.value.trim();
+    console.log("Email: " + inputEmail);
+  }
+
+  function validarEmail(email) {
+    let emailValue = /\S+@\S+\.\S+/;
+    if (!emailValue.test(email)) {
+      alert("Email inválido");
+      return false;
     }
+  }
 
-    function validarEmail(email) {
-        let emailValue = /\S+@\S+\.\S+/;
-        return emailValue.test(email);
+  inputs.forEach((input) => {
+    if (input.value.length > 0) {
+      buttonSubmit.disabled = false;
     }
+  });
 
-    buttonSubmit.addEventListener("click", function (e) {
-        normalizarCampos();
-        //Adicionar o "Se os dados existirem na base..."
-        if(validarEmail(inputEmail.value)){
-            window.location.href = "tarefas.html";
-            e.preventDefault();
-        } else{
-            console.log("Este email é inválido")
-        }
-    });
-})
+  buttonSubmit.addEventListener("click", function (e) {
+    normalizarCampos();
+    //Adicionar o "Se os dados existirem na base..."
+    if (validarEmail(inputEmail.value)) {
+      window.location.href = "tarefas.html";
+      e.preventDefault();
+    } else {
+      console.log("Este email é inválido");
+    }
+  });
 
+  event.preventDefault();
+});
 
+export { login };
---
+++ scripts/tasks.js
@@ -0,0 +1,47 @@
+import { login } from './login';
+
+const baseUrl = "https://ctd-todo-api.herokuapp.com";
+let jwt = login({
+    emailEntry: "asasasas@mail.com",
+    passwordEntry: "asaasd"
+});
+console.log(jwt)
+
+
+// Solicitação GET Tasks
+// fetch(`${baseUrl}/v1/tasks`)
+//     // Tratamento do sucesso
+//     .then((response) => response.json()) // converter para json
+//     .then((json) => console.log(json)) //imprimir dados no console
+//     .catch((err) => console.log("Erro de solicitação", err));
+
+//Envio POST Tasks
+let postTaskBody = {
+    description: "Terminar Checkpoint",
+    completed: "false",
+};
+
+function postTask(body) {
+    fetch(`${baseUrl}/v1/tasks`, {
+        method: "POST",
+        body: JSON.stringify(postTaskBody),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            "authorization": jwt,
+
+        },
+    })
+        .then((response) => {
+            if (response.status === 201) {
+                console.log("Successfull login");
+                return response.json()
+            }
+        })
+        .then((data) => {
+            console.log(data)
+            return data
+        })
+        .catch((err) => console.log(err));
+}
+
+//postTask(postTaskBody);
