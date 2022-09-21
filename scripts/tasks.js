62443427139fea4e9d49f65a213b231b93e53d44
Ariane França <ariane.franca@uberall.com>
2022-09-21 18:57:20 -0300

    adicionando taskCreator

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,34 +1,38 @@
 import { login, baseUrl } from "./login.js";
+let divTarefa = document.getElementById("divTarefa");
+// let inputDescricao = document.getElementById("descricaoPost");
+// let inputUrl = document.getElementById("urlImagemPost");
+// let buttonSubmit = document.getElementById("submitButton");
+// let form = document.querySelector("form");
 
 let token
 
 function getJwt() {
-  let jwt = login(
-     "denise@gmail.com",
-    "de123"
-  );
+    login(
+        "denise@gmail.com",
+        "de123"
+    );
 
-  token = localStorage.getItem("token");
-  console.log(`Retorno do getJwt(): ${token}`)
-  return token
+    token = localStorage.getItem("token");
+    console.log(`Retorno do getJwt(): ${token}`)
+    return token
 }
-
-
-
-
-
-// Solicitação GET Tasks
-// fetch(`${baseUrl}/v1/tasks`)
-//     // Tratamento do sucesso
-//     .then((response) => response.json()) // converter para json
-//     .then((json) => console.log(json)) //imprimir dados no console
-//     .catch((err) => console.log("Erro de solicitação", err));
 
 //Envio POST Tasks
 let postTaskBody = {
     description: "Terminar Checkpoint",
     completed: "false",
 };
+
+function transformaData() {
+    let data = new Date();
+    let ano = data.getFullYear();
+    let mes = data.getMonth() + 1;
+    let dia = data.getDate();
+    let dataFormatada = dia + "/" + mes + "/" + ano;
+
+    return `Criada em: ${dataFormatada}`;
+}
 
 function postTask(body) {
     fetch(`${baseUrl}/v1/tasks`, {
@@ -53,3 +57,49 @@ function postTask(body) {
 }
 
 postTask(postTaskBody);
+
+//<li class="tarefa">
+//   <div class="not-done"></div>
+//   <div class="descricao">
+//     <p class="nome">Nova tarefa</p>
+//     <p class="timestamp">Criada em: 15/07/21</p>
+//   </div>
+//</li> 
+
+function taskCreator(taskName) {
+    let liTarefa = document.createElement("li");
+    liTarefa.setAttribute("class", "tarefa");
+
+    let notDoneDiv = document.createElement("div");
+    notDoneDiv.setAttribute("class", "not-done");
+
+    let descricao = document.createElement("div");
+    descricao.setAttribute("class", "descricao");
+
+    let nomeTarefa = document.createElement("p");
+    nomeTarefa.setAttribute("class", "nome")
+    nomeTarefa.innerHTML = taskName;
+
+    let timeStamp = document.createElement("p");
+    timeStamp.setAttribute("class", "timeStamp")
+    timeStamp.innerHTML = transformaData();
+
+    descricao.appendChild(nomeTarefa)
+    descricao.appendChild(timeStamp)
+
+    liTarefa.appendChild(notDoneDiv);
+    liTarefa.appendChild(descricao);
+
+    document.getElementById("divTarefa").appendChild(liTarefa);
+    document.getElementById("divTarefa").appendChild(liTarefa);
+}
+
+taskCreator("Terminar Checkpoint");
+
+window.addEventListener("load", function (e) {
+    taskCreator("Terminar Checkpoint");
+
+    e.preventDefault();
+});
+
+
